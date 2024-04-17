9eeca0d463786d5f99ee2dd41f9d74fc992c720d
wno <winnyeinoo143@gmail.com>
2024-04-17 15:26:49 +0630

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>Customer page</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>Invoices page</p>
+    </div>
+  );
+};
+
+export default Page;
